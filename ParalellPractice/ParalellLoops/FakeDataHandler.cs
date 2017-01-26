ab7d41c17f48e290abe51ea1bc9fd3a9d5a35e7b
Raine_000 <Raine_000@RAINER-LAPTOP>
2017-01-26 15:46:25 +0100

    added basic loop tests

---
+++ ParalellPractice/ParalellLoops/FakeDataHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParalellLoops {
+    class FakeDataProvider {
+    public    void LoadFakeData() {
+            Thread.Sleep(2000);
+            Console.WriteLine("loaded Data");
+        }
+
+        public void ProcessFakeData() {
+            Thread.Sleep(1000);
+            Console.WriteLine("Processed Data");
+        }
+
+        public void SaveFakeData() {
+            Thread.Sleep(2000);
+            Console.WriteLine("saved Data");
+        }
+    }
+}
---
+++ ParalellPractice/ParalellLoops/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParalellLoops {
+    class Program {
+        private static Stopwatch sw;
+        private static FakeDataProvider dataProvider;
+        private static WaitHandle ready = new AutoResetEvent(false);
+        static void Main(string[] args) {
+            dataProvider = new FakeDataProvider();
+            IEnumerable<int> testData = Enumerable.Range(0, 10000);
+            object locker = new object();
+
+            Console.WriteLine("### sequencial ###");
+            sw = Stopwatch.StartNew();
+            long sum = testData.Sum();
+            sw.Stop();
+            Console.WriteLine(sum);
+            Console.WriteLine(sw.Elapsed);
+
+            Console.WriteLine("### paralell ###");
+            sw = Stopwatch.StartNew();
+            sum = testData.AsParallel().Sum();
+            sw.Stop();
+            Console.WriteLine(sum);
+            Console.WriteLine(sw.Elapsed);
+
+            sum = 0;
+
+            Console.WriteLine("### paralell divide ###");
+            sw = Stopwatch.StartNew();
+            Parallel.ForEach(testData, () => 0, (x, loopState, partialResult) => {
+                return x + partialResult;
+            }, (partialResult) => {
+                lock (locker) {
+                    sum += partialResult;
+                }
+            });
+
+            sw.Stop();
+            Console.WriteLine(sum);
+            Console.WriteLine(sw.Elapsed);
+
+            IList<Task> taskData = new List<Task>();
+
+            for (int i = 0; i < 20; i++) {
+                taskData.Add(new Task(() => {
+                    dataProvider.LoadFakeData();
+                    dataProvider.ProcessFakeData();
+                    dataProvider.SaveFakeData();
+                }));
+            }
+
+            Console.WriteLine("### Tasks sequential ###");
+            foreach (var item in taskData) {
+                item.Start();
+            }
+            Console.WriteLine("MainThread is sleeping");
+            Thread.Sleep(10000);
+            Console.WriteLine("MainThread continues");
+
+        }
+
+
+    }
+}
