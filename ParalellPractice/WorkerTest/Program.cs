971cd6f574db70144f68de1b7b43e25874af211f
Raine_000 <Raine_000@RAINER-LAPTOP>
2017-01-26 16:22:17 +0100

    Added WorkerTest which is playing around with Threads and their cancelBehavior

---
+++ ParalellPractice/WorkerTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkerTest {
+    class Program {
+        static void Main(string[] args) {
+            CancellationTokenSource source = new CancellationTokenSource();
+            Worker workerOutsideCancel = new Worker(1, source.Token);
+            Worker workerInsideCancel = new Worker(0);
+            workerOutsideCancel.Run();
+            workerInsideCancel.Run();
+            Thread.Sleep(5000);
+            source.Cancel();
+            Thread.Sleep(5000);
+            workerInsideCancel.Stop();
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ ParalellPractice/WorkerTest/Worker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkerTest {
+    class Worker {
+        Thread t;
+        CancellationTokenSource cancelSource;
+        CancellationToken cancel;
+        int initialValue;
+        public Worker(int initialvalue,  CancellationToken token) {
+            this.initialValue = initialvalue;
+            cancel = token;
+        }
+
+        public Worker(int initialValue) {
+            this.initialValue = initialValue;
+            cancelSource = new CancellationTokenSource();
+            cancel = cancelSource.Token;
+        }
+
+
+        public void Run() {
+            t = new Thread(() => {
+                Console.WriteLine("Worker is working really hard!");
+                var value = initialValue;
+                while (true) {
+                    if (cancel.IsCancellationRequested)
+                        return;
+                    Console.WriteLine(value++);
+                    Thread.Sleep(1000);
+                }
+            });
+            t.Start();
+        }
+
+        public void Stop() {
+            if (cancelSource == null)
+                return;
+            cancelSource.Cancel();
+        }
+    }
+}
